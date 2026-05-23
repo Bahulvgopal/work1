18a1c8071bd16cb1b7f5fb1a3bb5769a8eb55134
Bahulvgopal <145193440+Bahulvgopal@users.noreply.github.com>
2026-05-23 07:33:20 +0530

    Update event time from '10 AM Onwards' to 'After 12 Noon'

--- components/EventSchedule.tsx
+++ components/EventSchedule.tsx
@@ -103,7 +103,7 @@ const events = [
     title: "Food & Dinning",
     subtitle: "Post-Wedding",
     date: "02 July 2026",
-    time: "10 AM Onwards",
+    time: "After 12 Noon",
     venue: "Alsaj International Convention Centre",
     hall: "Gravity Hall, Kazhakottam",
     icon: (
